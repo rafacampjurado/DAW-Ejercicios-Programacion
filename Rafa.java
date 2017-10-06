652dfa21df2af2f867dcee39c44d184ac33c6bc4
Rafael Campos Jurado <rcj.es1997@gmail.com>
2017-10-06 09:23:38 +0200

    Colores

--- Rafa.java
+++ Rafa.java
@@ -8,14 +8,8 @@ public class Rafa { // Clase principal
   public static void main(String[] args) {
     System.out.println("\033[31m ¡Hola, soy Rafa! \nDirección: blablablablabla\nTeléfono: 111111");
     System.out.println("\033[34m Programa hecho por Rafael Campos");
-	 
-	String rojo = "\033[31m";
-    String verde = "\033[32m";
-    String naranja = "\033[33m";
-    String azul = "\033[34m";
-    String morado = "\033[35m";
-    String blanco = "\033[37m";
-    String celeste = "\033[36m";
+    System.out.println("\033[32m Prueba para las etiquetas de versiones");
+	
 	
   }
  
