3a29e6589fc427ced0305d421ca248cd5dd62019
rafacampjurado <rcj.es1997@gmail.com>
2017-10-19 11:02:20 +0200

    ejercicos

--- t4/actv11.java
+++ t4/actv11.java
@@ -11,6 +11,13 @@
 public class actv11 {
 	
 	public static void main (String args[]) {
+		System.out.print("Introduce la hora y los minutos, separados con un punto: " );
+		String preguntaHora;
+		preguntaHora = System.console().readLine();
+		double hora = Double.parseDouble(preguntaHora);
+		double calculoHora = (23.59 - hora)* 3600;
+		System.out.print("Quedan "+ calculoHora +" segundos para la Media Noche");
+		
 		
 	}
 }
