37608fecd01d56311075ef3768da4f24004f7dc2
rafacampjurado <rcj.es1997@gmail.com>
2017-10-25 18:27:31 +0200

    ejercicio 15 terminado

--- t4/Actv16.java
+++ t4/Actv16.java
@@ -22,26 +22,96 @@ if (respuesta.equals("verdadero")) {
 puntuacion ++;puntuacion ++;puntuacion ++;
 System.out.print(puntuacion);
 } else {
-  System.out.print(puntuacion);
+  //~ System.out.print(puntuacion);
 }
-//~ 2. Ha aumentado sus gastos de vestuario
-//~ 3. Ha perdido el interés que mostraba anteriormente por ti
-//~ 4. Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora se arregla el pelo y se asea con más frecuencia (si es mujer)
-//~ 5. No te deja que mires la agenda de su teléfono móvil
-//~ 6. A veces tiene llamadas que dice no querer contestar cuando estás tú delante
-//~ 7. Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a
-//~ 8. Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo
-//~ 9. Has notado que últimamente se perfuma más
-//~ 10. Se confunde y te dice que ha estado en sitios donde no ha ido contigo
+System.out.print("2. ¿Ha aumentado sus gastos de vestuario?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals("verdadero")) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
 
-//~ Puntuación entre 0 y 10:
-//~ ¡Enhorabuena! tu pareja parece ser totalmente fiel.
+System.out.print("3. ¿Ha perdido el interés que mostraba anteriormente por ti?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+System.out.print("4. ¿Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora se arregla el pelo y se asea con más frecuencia (si es mujer)?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+
+System.out.print("5. ¿No te deja que mires la agenda de su teléfono móvil?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+System.out.print("6. ¿A veces tiene llamadas que dice no querer contestar cuando estás tú delante?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+System.out.print("7. ¿Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+System.out.print("8. ¿Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+System.out.print("9. ¿Has notado que últimamente se perfuma más?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+System.out.print("10. ¿Se confunde y te dice que ha estado en sitios donde no ha ido contigo?: ");
+respuesta = System.console().readLine().toLowerCase();
+if (respuesta.equals(puntuacion)) {
+puntuacion ++;puntuacion ++;puntuacion ++;
+} else {
+  //~ System.out.print(puntuacion);
+}
+
+if ((puntuacion <=0)&&(puntuacion ==10)) {
+  System.out.print("¡Enhorabuena! tu pareja parece ser totalmente fiel.");
+}
+else if ((puntuacion >= 11 )&&(puntuacion <= 22)) {
+  System.out.print("Quizás exista el peligro de otra persona en su vida o en su mente, aunque seguramente será algo sin importancia. No bajes la guardia.");
+}
+else if ((puntuacion >= 22)&&(puntuacion <= 30)) {
+  System.out.print("Tu pareja tiene todos los ingredientes para estar viviendo un romance con otra persona. Te aconsejamos que indagues un poco más y averigües que es lo que está pasando por su cabeza.");
+}
+
 
 //~ Puntuación entre 11 y 22:
-//~ Quizás exista el peligro de otra persona en su vida o en su mente, aunque seguramente será algo sin importancia. No bajes la guardia.
+//~ 
 
 //~ Puntuación entre 22 y 30
-//~ Tu pareja tiene todos los ingredientes para estar viviendo un romance con otra persona. Te aconsejamos que indagues un poco más y averigües que es lo que está pasando por su cabeza.
+//~ 
 }
 }
 
