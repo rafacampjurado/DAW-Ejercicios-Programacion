05058c829afd4ea888eac32a3c904e5b6aeca621
rafacampjurado <rcj.es1997@gmail.com>
2017-10-26 18:46:04 +0200

    ejercicio 2 del tema 5

---
+++ t5/Actv02.java
@@ -0,0 +1,27 @@
+/*
+ * Actv02.java
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while .
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv02 {
+public static void main (String args[]) {
+//~ int mult = 1;
+//~ int resultado = num * mult;
+//~ for (int i = 1; i <= 100; i++) {
+     //~ System.out.println(i);
+     //~ System.out.println(i*5);
+  //~ }
+  int i = 0;
+    while (i <= 100) {
+      System.out.println(i*5);
+      i++;
+    }
+
+
+ }
+}
+
