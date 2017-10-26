72e7c47691345892ea0fa8ba89e78609e12d3b00
rafacampjurado <rcj.es1997@gmail.com>
2017-10-26 18:51:48 +0200

    ejercicios del tema 5

---
+++ t5/Actv03.java
@@ -0,0 +1,25 @@
+/*
+ * Actv03.java
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while .
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv03 {
+public static void main (String args[]) {
+//~ int mult = 1;
+//~ int resultado = num * mult;
+//~ for (int i = 1; i <= 100; i++) {
+     //~ System.out.println(i);
+     //~ System.out.println(i*5);
+  //~ }
+  int i = 0;
+    do  {
+      System.out.println(i*5);
+      i++;
+    } while (i <= 100);
+ }
+}
+
