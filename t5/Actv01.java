2e36e6c423dfa61aa34a870fc4321982b5fec444
rafacampjurado <rcj.es1997@gmail.com>
2017-10-26 18:38:40 +0200

    ejercicio 1 del tema 5

---
+++ t5/Actv01.java
@@ -0,0 +1,22 @@
+/*
+ * Actv01.java
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for .
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv01 {
+public static void main (String args[]) {
+//~ int mult = 1;
+//~ int resultado = num * mult;
+for (int i = 1; i <= 100; i++) {
+     //~ System.out.println(i);
+     System.out.println(i*5);
+  }
+
+
+ }
+}
+
