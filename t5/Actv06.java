36e3dbfe3b7530773c2c2215bc24aa18eba3c1f5
rafacampjurado <rcj.es1997@gmail.com>
2017-10-26 19:32:58 +0200

    ejercicios del tema 5

---
+++ t5/Actv06.java
@@ -0,0 +1,22 @@
+/*
+ * Actv06.java
+ * 
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+ * utilizando un bucle do-while .
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv06 {
+public static void main (String args[]) {
+  int i = 320;
+  do {
+    i= i-20;
+    System.out.println(i);
+  } while (i >160);
+   
+ }
+}
+
+
