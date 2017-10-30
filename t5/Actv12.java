7aba8752b093a09f57db8b4a343dac77653b4d80
rafacampjurado <rcj.es1997@gmail.com>
2017-10-30 14:19:49 +0100

    ejercicio 12

---
+++ t5/Actv12.java
@@ -0,0 +1,41 @@
+/*
+ * Actv12.java
+ * 
+ * Escribe un programa que muestre los n primeros términos de la serie de
+ * Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1
+ * y el resto se calcula sumando los dos anteriores, por lo que tendríamos que
+ * los términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144... El número n se
+ * debe introducir por teclado
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv12 {
+public static void main (String args[]) {
+   int primerNumero;
+   int contador = 0;
+   int suma = 0;
+   
+  System.out.print("Introduce un número: ");
+  primerNumero = Integer.parseInt(System.console().readLine());
+  //~ System.out.print(" "+primerNumero);
+  
+  while (contador <20){
+    //~ System.out.print(" "+primerNumero);
+    suma += primerNumero;
+    System.out.print(" "+suma);
+    primerNumero++;
+    contador++;
+   }
+    
+  
+ 
+  
+  }
+}
+
+  
+  
+
+
