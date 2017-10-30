f079d939796df942967a64e0cd72362f4fa2faf8
rafacampjurado <rcj.es1997@gmail.com>
2017-10-30 16:39:22 +0100

    ejercicios del tema 4

--- t4/Actv17.java
+++ t4/Actv17.java
@@ -10,8 +10,8 @@
 public class  Actv17 {
  public static void main (String args[]) {
   System.out.print("Introduce el número entero: ");
-  int numero = Interger.parseInt(System.console().readLine());
-  if (numero)
+  int numero = Integer.parseInt(System.console().readLine());
+  System.out.println("La última cifra del número introducido es el " + (numero % 10));
  }
 }
 
