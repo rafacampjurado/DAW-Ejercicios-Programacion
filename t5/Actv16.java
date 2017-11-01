8a1598bca4bd0e1a503fd353b9cb12beab9437cf
rafacampjurado <rcj.es1997@gmail.com>
2017-11-01 19:49:40 +0100

    ejercicio 17

--- t5/Actv16.java
+++ t5/Actv16.java
@@ -13,16 +13,23 @@ public class Actv16 {
 public static void main (String args[]) {
   System.out.print("Introduce un número: ");
   int numPrimo = Integer.parseInt(System.console().readLine());
-  int divisor = 1;
-  //~ int division = numPrimo / divisor;
+  //~ int divisor;
+  int divisor = 2;
+  int division = numPrimo / divisor;
   
-  do {
-    int division = numPrimo / divisor;
+  while (division >0){
+  
+  division = numPrimo / divisor;
+  System.out.println(" "+division);
+  
+  if (division == 0){
+    System.out.print("el número "+numPrimo+" es primo");
+  }else{
     divisor++;
+    }
     
-    } while (division > 0);
-  
-  }
+ } 
+}
 }
 
 
---
+++ t5/Actv17.java
@@ -0,0 +1,29 @@
+/*
+ * Actv14¡7.java
+ * 
+ * Realiza un programa que sume los 100 números siguientes a un número entero
+ * y positivo introducido por teclado. Se debe comprobar que el dato introducido
+ * es correcto (que es un número positivo).
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv17 {
+public static void main (String args[]) {
+  System.out.print("Introduce un número entero: ");
+  int numeroIntroducido = Integer.parseInt(System.console().readLine());
+  int multiplicador = 1;
+  multiplicador = multiplicador * numeroIntroducido;
+  for (int contador = 0;contador <= 100;contador++){
+    System.out.print("|"+(multiplicador + contador)+"|");
+  }
+  
+  }
+}
+
+
+  
+  
+
+
