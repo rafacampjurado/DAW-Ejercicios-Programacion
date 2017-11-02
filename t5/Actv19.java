4b3f52fa43b77b45650c6be3b6c154eca43bfca4
rafacampjurado <rcj.es1997@gmail.com>
2017-11-02 09:57:20 +0100

    arreglando el gitignore

---
+++ t5/Actv19.java
@@ -0,0 +1,27 @@
+/*
+ * Actv19.java
+ * 
+ * Realiza un programa que pinte una pirámide por pantalla. La altura se debe
+ * pedir por teclado. El carácter con el que se pinta la pirámide también se debe
+ * pedir por teclado.
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv19 {
+public static void main (String args[]) {
+  System.out.print("Introduce un número entero: ");
+  int A = Integer.parseInt(System.console().readLine());
+  
+  System.out.print("Introduce un segundo  número entero: ");
+  int B = Integer.parseInt(System.console().readLine());
+  
+  }
+}
+
+
+  
+  
+
+
