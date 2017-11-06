6b0ea78881c62e3f4a04f7d287b2ff60a33e03b0
rafacampjurado <rcj.es1997@gmail.com>
2017-11-06 17:42:06 +0100

    ejercicio 24 con dudas y comienzo del 25

---
+++ t5/Actv24.java
@@ -0,0 +1,31 @@
+/*
+ * Actv24.java
+ * 
+ * Escribe un programa que lea un número "n" e imprima una pirámide de números
+ * con "n" filas como en la siguiente figura:
+ *    1
+ *   121
+ *  12321
+ * 1234321
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv24 {
+public static void main (String args[]) {
+
+
+
+    }
+    
+  }
+  
+ 
+
+
+
+  
+  
+
+
---
+++ t5/Actv25.java
@@ -0,0 +1,22 @@
+/*
+ * Actv25.java
+ * 
+ * Escribe un programa que lea un número "n" e imprima una pirámide de números
+ * con "n" filas como en la siguiente figura:
+ *    1
+ *   121
+ *  12321
+ * 1234321
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv25 {
+public static void main (String args[]) {
+
+
+
+}
+    
+ }
