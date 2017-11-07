17f489f3b7bc56dbe83eaa5ffec68a922c6d3fc1
rafacampjurado <rcj.es1997@gmail.com>
2017-11-07 21:11:45 +0100

    ejercicio 28

---
+++ t5/Actv28.java
@@ -0,0 +1,30 @@
+/*
+ * Actv28.java
+ * 
+ * Escribe un programa que calcule el factorial de un número entero leído por
+ * teclado.
+ * Ejemplo:
+ * Por favor, introduzca un número entero: 6
+ * 6! = 720
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv28 {
+ 
+ public static void main (String args[]) {
+   System.out.print("Introduce un número: ");
+    int numeroIntroduccido = Integer.parseInt(System.console().readLine());
+    int numero = numeroIntroduccido;
+    int suma = 0;
+    int mult = 0;
+    
+    for (int i = (numeroIntroduccido - 1); i >0; i--) {
+      numero = i * numero ;
+      numero = numero + mult;
+    }
+    System.out.println(+ numeroIntroduccido+"! = "+numero);
+  }
+}
+
