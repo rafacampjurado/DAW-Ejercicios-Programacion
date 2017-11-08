3eb8c41f62fc3edc288bee9e6288b1551b328d02
rafacampjurado <rcj.es1997@gmail.com>
2017-11-08 18:22:47 +0100

    ejercicio 30

---
+++ t5/Actv30.java
@@ -0,0 +1,31 @@
+/*
+ * Actv30.java
+ * 
+ * Realiza una programa que calcule las horas transcurridas entre dos horas de
+ * dos días de la semana. No se tendrán en cuenta los minutos ni los segundos.
+ * El día de la semana se puede pedir como un número (del 1 al 7) o como una
+ * cadena (de “lunes” a “domingo”). Se debe comprobar que el usuario introduce
+ * los datos correctamente y que el segundo día es posterior al primero.
+ * Ejemplo:
+ * Por favor, introduzca la primera hora.
+ * Día: lunes
+ * Hora: 18
+ * Por favor, introduzca la segunda hora.
+ * Día: martes
+ * Hora: 20
+ * Entre las 18:00h del lunes y las 20:00h del martes hay 26 hora/s.
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv30 {
+ 
+ public static void main (String args[]) {
+   System.out.print("Introduce el número acorde al día actual de la semana: ");
+   int dia = Integer.parseInt(System.console().readLine();
+   
+   
+
+  }
+}
+
