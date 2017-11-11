4306572d596998e046aa83b0ff385e0f8310a46d
rafacampjurado <rcj.es1997@gmail.com>
2017-11-11 19:52:46 +0100

    ejercicio 32

---
+++ t5/Actv32.java
@@ -0,0 +1,63 @@
+/*
+ * Actv32.java
+ * 
+ * Escribe un programa que, dado un número entero, diga cuáles son y cuánto
+ * suman los dígitos pares. Los dígitos pares se deben mostrar en orden, de
+ * izquierda a derecha. Usa long en lugar de int donde sea necesario para admitir
+ * números largos.
+ * 
+ * Por favor, introduzca un número entero positivo: 94026782
+ * Dígitos pares: 4 0 2 6 8 2
+ * Suma de los dígitos pares: 22
+ * 
+ * @author Rafael Campos Jurado
+ */
+
+
+public class Actv32 {
+ 
+ public static void main (String args[]) {
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
+
+    // Le da la vueta al número y calcula la longitud
+    long numero = numeroIntroducido;
+    long volteado = 0;
+    int longitud = 0;
+    
+    if (numero == 0) {
+      longitud = 1;
+    }
+    
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+      longitud++;
+    } // while
+
+    // Muestra los dígitos pares
+    System.out.print("Dígitos pares: ");
+
+    int digito;
+    int sumaPares = 0;
+    
+    for (int i = 0; i < longitud; i++) {
+
+      digito = (int)(volteado % 10);
+
+      if ((digito % 2) == 0) {
+        System.out.print(digito + " ");
+        sumaPares += digito;
+      }
+
+      volteado /= 10;
+    }
+    
+    // Muestra la suma de los dígitos pares
+    System.out.println("\nSuma de los dígitos pares: " + sumaPares);
+  }
+}
+
+
+
+
