b6f85517e37207cdcab7ddf8791a29197fbdb0b3
Rafael Campos Jurado <rcj.es1997@gmail.com>
2017-11-27 16:52:04 +0100

    actv 14

---
+++ t6/Actv14.java
@@ -0,0 +1,48 @@
+//Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6.
+//El programa intentará adivinar el número que estás pensando - un número
+//entre 0 y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el
+//programa debe preguntar si el número que estás pensando es mayor o menor
+//que el que te acaba de decir.
+// * @author Rafael Campos Jurado
+// */
+package actividades;
+
+import java.util.Scanner;
+public class Actv14{
+  public static void main(String[] args) {  
+    Scanner s = new Scanner (System.in);
+    int oportunidades = 5;
+    int numeroPensado;
+    int minimo = 0;
+    int maximo = 100;
+    boolean acertado = false;
+    int mayorMenorOIgual;
+        
+    System.out.println("Piensa un número del 0 al 100. Intentaré adivinarlo en 5 intentos.");
+    System.out.println("Pulsa la tecla INTRO cuando estés preparado.");
+    s.nextLine();
+
+    do {
+      numeroPensado = (int)(Math.random() * (maximo - minimo) + minimo);
+      System.out.println("¿Es el " + numeroPensado + "?");
+      System.out.print("El número que estás pensando es 1) mayor 2) menor 3) el mismo: ");
+      mayorMenorOIgual = s.nextInt();
+      oportunidades--;
+
+      if ( (mayorMenorOIgual == 1) && (oportunidades > 0) )
+        minimo = numeroPensado + 1;
+      
+      if ( (mayorMenorOIgual == 2) && (oportunidades > 0) )
+        maximo = numeroPensado - 1;
+      
+      if (mayorMenorOIgual == 3) {
+        acertado = true;
+        System.out.println("¡Bien! ¡he acertado!");
+      }
+    } while(!acertado && (oportunidades > 0));
+    
+    if (!acertado) {
+      System.out.println("Vaya, no he conseguido acertar el número.");
+    }
+  }
+}
