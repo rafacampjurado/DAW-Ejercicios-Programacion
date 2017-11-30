1fe0f94f0a528ebc987f73872b2f6ced42d69572
unknown <rcj.es1997@gmail.com>
2017-11-30 18:20:03 +0100

    ejercicio 5 tema 7

--- t7/Actv5.java
+++ t7/Actv5.java
@@ -5,28 +5,46 @@
 package array;
 
 import java.util.Scanner;
+
 public class Actv5 {
 
-  public static void main(String[] args) {
-    int [] numero = new int [10];
-    Scanner s = new Scanner (System.in);
-    int aux1 = 0;
-    int aux2 = 0;
-    
+    public static void main(String[] args) {
+        int[] numero = new int[10];
+        Scanner s = new Scanner(System.in);
+        int aux1 = 0;
+        int aux2 = 1;
+
 //    INICIO PEDIR DATOS
-    for (int i = 0; i < 10; i++) {
-      System.out.print("Introduce 10 números: ");
-      numero [i] = s.nextInt();
-    }
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Introduce 10 números: ");
+            numero[i] = s.nextInt();
+        }
 //    FIN PEDIR DATOS
-    for (int i = 0; i < 10; i++) {
-      System.out.print(numero[i]);
+
+        for (int i = 0; i < 10; i++) {
+//            System.out.print(numero[i]+ " ");
+            if (numero[i] > aux1) {
+                aux1 = numero[i];
+            }
+            if (numero [i] <= aux2) {
+                aux2 = numero [i];
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if (numero[i] == aux1) {
+                System.out.print("MAX");
+            } else{
+                
+            }
+            if (numero [i] == aux2) {
+                System.out.print("MIN");
+            }else {
+                
+            }
+            System.out.println(" "+numero[i]);
+        }
+//        System.out.println("");
+//        System.out.println("MAX "+ aux1);
+//        System.out.println("MIN "+ aux2);
     }
-    for (int i = 0; i < 10; i++) {
-      numero[i] = aux1;
-      if (aux1 > numero[i]) {
-        System.out.print(numero+"MAX");
-      }
-    }
-  }
 }
